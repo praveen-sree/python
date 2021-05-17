b3435e92143241ef0d81e9ee39b7f44bb2ea5351
praveen-sree <praveen.sree@gmail.com>
2021-05-17 09:00:00 +0530

    Update custom_waiters.py

--- custom_waiters.py
+++ custom_waiters.py
@@ -1,9 +1,13 @@
+# Refeerences 
+# https://www.2ndwatch.com/blog/use-waiters-boto3-write/
+# https://docs.aws.amazon.com/code-samples/latest/catalog/python-demo_tools-custom_waiter.py.html
+
 def waiter_for_replication_task_status(replication_task_arn :'str', Region_name: 'str'):
     # "operation": "DescribeReplicationTasks(Filters=[{'Name': 'replication-task-arn','Values': [replication_task_arn]}])",
     logger.info('inside waiter_for_replication_task_status')
     dms = boto3.client("dms", Region_name)
     waiter_name = "ReplicationTaskReadyStopped"
-    delay = 5
+    delay = 3
     max_attempts = 12
     waiter_config = {
         "version": 2,
@@ -62,7 +66,7 @@ def waiter_for_instance_task_status(cluster_id :'str', Region_name: 'str'):
     custom_waiter = create_waiter_with_client(waiter_name, waiter_model, dms)
     logger.info("Inside WAITER CLASS DEFINITION")
     try:
-        inst_id="a200333-drcoreservicepg0657-dev-pg-cluster-us-east-1-inst"
+        inst_id="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-us-east-1-inst"
         logger.info(inst_id)
         # custom_waiter.wait(DBInstanceIdentifier=inst_id)
         custom_waiter.wait(Filters = [{'Name': 'db-instance-id','Values': [inst_id]}])
